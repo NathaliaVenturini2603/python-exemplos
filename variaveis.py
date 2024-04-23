cfe90708c3851e9b77f15086e00c516b99866adb
NathaliaVenturini2603 <venturininathalia275@gmail.com>
2024-04-23 16:44:24 -0300

    criação variaveis.py

---
+++ variaveis.py
@@ -0,0 +1,12 @@
+nome="Nathalia Venturini"
+idade=16
+email='nathaliaventurini.nv@gmail.com'
+telefone='17 99656-1542'
+nota=9.55
+aprovado=True
+print(nome)
+print(idade)
+print(email)
+print(telefone)
+print(nota)
+print(aprovado)
