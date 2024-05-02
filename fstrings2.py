f51b338728a9740fe1f328e000fbcdcdbcaa9b32
NathaliaVenturini2603 <venturininathalia275@gmail.com>
2024-05-02 16:50:59 -0300

    atualização fstrings2.py

--- fstrings2.py
+++ fstrings2.py
@@ -2,6 +2,10 @@
 escola = 'Senai'
 curso = 'Desenvolvimento de Sistemas'
 uc = 'Logica de Programação'
+matricula = 34
+nota = 8.99999
 print(f"Escola: {escola}\n"
     f"Curso: {curso}\n"
-    f"Unidade Curricular: {uc}\n")
+    f"Unidade Curricular: {uc}\n"
+    f"Matricula: {matricula:06d}\n"
+    f"Nota: {nota:.2f}")
