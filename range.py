00ede1584c256d540445524980a9b3e0efdac060
NathaliaVenturini2603 <venturininathalia275@gmail.com>
2024-05-16 09:10:30 -0300

    atualização range.py

--- range.py
+++ range.py
@@ -1,5 +1,6 @@
-# Função Range
+# Fução Range
 print(list(range(3)))
 print(list(range(10, 16)))
 print(list(range(10, 16, 2)))
-print(list())
+print(list(range(16, 10, -1)))
+print(list(range(16, 10, -2)))
